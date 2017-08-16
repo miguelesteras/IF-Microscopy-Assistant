45d97a67243b7da3138a35c8171863bc321bfea8
miguelesteras <miguelesteras@gmail.com>
2017-08-15 20:28:41 -0700

    fix bug in binary search

--- code/transform_Boundary.m
+++ code/transform_Boundary.m
@@ -44,35 +44,33 @@ for i = 1:num_files
     BoundaryDataSet = cell(100,seqLength); 
     count = 1;
     
-    % the black canvas where to draw the dynamic image has dimensions given
-    % by the max radious detected in sample, +1 pixel. This makes is square
-    % and containing a central pixel (center of mass).
     for j = 1:size(idx,1)
         idx2 = find(~cellfun(@isempty,cellCoordenates(idx(j),:)));
         for k = 1:size(idx2,2) - (seqLength-1)
-            rotation = rotationUp{idx(j),idx2(k)} + pi/2;   % +pi/2 added so rotation points west
+            rotation = rotationUp{idx(j),idx2(k)} - pi/2;   % -pi/2 added so rotation points west
             input = int8(zeros((metadata.maxRadious*2)+1, (metadata.maxRadious*2)+1));
             for m = 1:seqLength                
                 selection = cellCoordenates{idx(j),idx2(k+m-1)};
                 selection(:,1) = selection(:,1) + rotation;          % apply rotation
                 [x,y] = pol2cart(selection(:,1),selection(:,2));
-                contour = [round(x) round(y)];
-                % Binary Search algorithm implementation
-                from = 0;
-                to = 1;
-                s = 0.5;
-                sumInd = boundary(contour,s);
+                cell = [round(x) round(y)];
+                
+                % Start of Binary Search algorithm implementation for
+                % values of the shrinking factor 's'
+                from = 0; to = 1; s = 0.5;
+                sumInd = boundary(cell,s);
                 while numel(sumInd) ~= vecSize
                     if numel(sumInd) < vecSize
                         from = s;
-                        s = round((s + to)/2);                        
+                        s = (s + to)/2;                        
                     elseif numel(sumInd) > vecSize 
                         to = s;
-                        s = round((s + from)/2); 
+                        s = (s + from)/2; 
                     end
-                    sumInd = boundary(contour,s);
+                    sumInd = boundary(cell,s);
                 end
-                boundary = contour(sumInd(1:end-1),:);        % remove last value (because it is equal to first)
+                
+                boundary = cell(sumInd(1:end-1),:);        % remove last value (because it is equal to first)
                 BoundaryDataSet{count,m} = boundary;
             end           
             count = count+1;
@@ -86,25 +84,23 @@ end
 
 %%
 
-I = imrotate(imfill(dynamicTarget{1}),90);  % further way point from center faced west, so pixel boundary starts from it and follows clockwise
-%I = imfill(dynamicTarget{1});
-stats = regionprops(I,'PixelList');
-c = stats.PixelList;
-no = numel(c);
-k = boundary(c,0.7);
-summary = c(k(1:end-1),:); % remove last value (because it is equal to first)
-
 figure
-ind = sub2ind([193,193], c(:,2), c(:,1));
-canvas = false(193,193);
+
+ind = sub2ind([200,200], contour(:,2)+100, contour(:,1)+100);
+canvas = false(200,200);
 canvas(ind) = true;
-imshowpair(I,canvas,'montage');
 
-figure
-indSu = sub2ind([193,193], summary(1:50,2), summary(1:50,1));
-canvasSu = false(193,193);
-canvasSu(indSu) = true;
-imshowpair(I,canvasSu,'montage');
+ind2 = sub2ind([200,200], cell(:,2)+100, cell(:,1)+100);
+canvas2 = false(200,200);
+canvas2(ind2) = true;
+
+imshowpair(canvas,canvas2,'montage');
+% 
+% figure
+% indSu = sub2ind([193,193], summary(1:50,2), summary(1:50,1));
+% canvasSu = false(193,193);
+% canvasSu(indSu) = true;
+% imshowpair(I,canvasSu,'montage');
 
 % figure
 % plot(c(:,1),c(:,2),':','LineWidth',8,'Color',[.9 .9 .9]); hold on;
