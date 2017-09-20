682f2c4b52780f3a3dc0845bea654942d184826d
miguelesteras <miguelesteras@gmail.com>
2017-09-20 19:41:21 +0100

    feedfor net

--- 2_code_training/train_FeedForward.m
+++ 2_code_training/train_FeedForward.m
@@ -10,85 +10,83 @@
 %   2. Train an FeedForward Network from Rho descriptor vectors
 %   ======================================================================
 
-%% 1. Train an FeedForward Network from Fourier Descriptors
+% 1. Train an FeedForward Network from Fourier Descriptors
+clear; close all; clc;
 rng('default')
 
-% Create a joined dataSet from all video files
-files = dir('*_metadata.mat');      
-num_files = length(files);
-inputData = [];   
-targetData = [];
-for i = 1:num_files
-    load(files(i).name,'metadata');                               
-    load(strcat(metadata.name,'_fourierInput.mat'),'fourierInput');     
-    load(strcat(metadata.name,'_fourierTarget.mat'),'fourierTarget');     
-    inputData = [inputData; fourierInput];
-    targetData = [targetData; fourierTarget];
-end
+load('fourierDescriptorC4.mat','fourierDescriptor');
+Input = fourierDescriptor(:,1:end-2);
+Output = fourierDescriptor(:,end-1);
+
+seqLen = size(Input,2);
+samples = size(Input,1);
+maxSam = floor(samples*0.85);
 
 % transform input and output from cell array to column vectors
-X = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),inputData,'UniformOutput',false)))';
-Y = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),targetData,'UniformOutput',false)))';
+X = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),Input,'UniformOutput',false)))';
+Y = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),Output,'UniformOutput',false)))';
 
 % Training feedforward network
-hiddenSizes = [500 200];                          % neurons in hidden layer
-ffNet = feedforwardnet(hiddenSizes);        % create feedforward network
-view(ffNet)
+hiddenCell = {100, [150 100], [150 100 50]};    
 
-% Fine-tunning
-ffNet.trainFcn                    = 'trainoss';     % One-step secant backpropagation
-ffNet.divideFcn                   = 'dividerand';   % data division random
-ffNet.divideParam .trainRatio     = 0.9;              % all data used for training
-ffNet.divideParam.valRatio        = 0.1;
-ffNet.divideParam.testRatio       = 0;
-ffNet.trainParam.epochs           = 1000;           % maximum number of epochs to train (for early stopping)
-ffNet.trainParam.showCommandLine  = false;          % do not generate command-line output
-ffNet.trainParam.showWindow       = true;           % show training GUI
-ffNet.trainParam.max_fail         = 10;             % maximum validation failures (for early stopping)
-ffNet.trainParam.show             = 1;              % Epochs between displays
-tic;                                                % start timer
+for h = 1:numel(hiddenCell)
+    hiddenSizes = hiddenCell{h};
+    net = feedforwardnet(hiddenSizes);        % create feedforward network
+    % Fine-tunning
+    net.trainFcn                    = 'trainoss';     % One-step secant backpropagation
+    net.divideFcn                   = 'dividerand';   % data division random
+    net.divideParam .trainRatio     = 0.85;              % all data used for training
+    net.divideParam.valRatio        = 0.15;
+    net.divideParam.testRatio       = 0;
+    net.trainParam.epochs           = 100;           % maximum number of epochs to train (for early stopping)
+    net.trainParam.showCommandLine  = false;          % do not generate command-line output
+    net.trainParam.showWindow       = true;           % show training GUI
+    net.trainParam.max_fail         = 10;             % maximum validation failures (for early stopping)
+    net.trainParam.show             = 1;              % Epochs between displays
+    % train net
+    [net,record] = train(net,X,Y);
+    % save network and performance
+    save(strcat('net_feedfor_Fourier',num2str(h),'.mat'),'net')
+    save(strcat('perf_feedfor_Fourier',num2str(h),'.mat'),'record')
+end
 
-[ffNet,FFrecord] = train(ffNet,X,Y);
-sprintf('Training time: %0.0f %s',toc,'s')          % duration of training
 
+% 2. Train an FeedForward Network from Rho Descriptors
 
-%% 2. Train an FeedForward Network from Rho descriptors
-rng('default')
+clear;
 
-load('RhoDescriptors.mat','RhoDescriptors');   
+load('RhoDescriptors.mat','RhoDescriptors');
+Input = RhoDescriptors(:,1:end-1);
+Output = RhoDescriptors(:,end);
+
+seqLen = size(Input,2);
+samples = size(Input,1);
+maxSam = floor(samples*0.85);
 
 % transform input and output from cell array to column vectors
-X = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),RhoDescriptors(:,1:end-1),'UniformOutput',false)))';
-Y = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),RhoDescriptors(:,end),'UniformOutput',false)))';
+X = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),Input,'UniformOutput',false)))';
+Y = (cell2mat(cellfun(@(x) (reshape(x', 1, [])),Output,'UniformOutput',false)))';
 
-% create training and test set.
+% Training feedforward network
+hiddenCell = {100, [150 100], [150 100 50]};    
 
-testRatio = .15;
-testIdx = randi([1 size(X,2)],1,round(size(X,2)*testRatio));
-Xtest = X(:,testIdx);
-Ytest = Y(:,testIdx);
-X(:,testIdx) = [];
-Y(:,testIdx) = [];
-
-% Create feedforward network with two hidden layer 
-hiddenSizes = [300 300];                          
-ffNet = feedforwardnet(hiddenSizes);     
-view(ffNet)
-
-% Hyper-parameters of network
-ffNet.trainFcn                    = 'trainoss';     % One-step secant backpropagation
-ffNet.divideFcn                   = 'dividerand';   % data division random
-ffNet.divideParam .trainRatio     = 0.9;              % all data used for training
-ffNet.divideParam.valRatio        = 0.1;
-ffNet.divideParam.testRatio       = 0;
-ffNet.trainParam.epochs           = 1000;           % maximum number of epochs to train (for early stopping)
-ffNet.trainParam.showCommandLine  = false;          % do not generate command-line output
-ffNet.trainParam.showWindow       = true;           % show training GUI
-ffNet.trainParam.max_fail         = 10;             % maximum validation failures (for early stopping)
-ffNet.trainParam.show             = 1;              % Epochs between displays
-tic;                                                % start timer
-
-% train network on data
-[ffNet,FFrecord] = train(ffNet,X,Y);
-sprintf('Training time: %0.0f %s',toc,'s')          % duration of training
-
+for h = 1:numel(hiddenCell)
+    hiddenSizes = hiddenCell{h};
+    net = feedforwardnet(hiddenSizes);        % create feedforward network
+    % Fine-tunning
+    net.trainFcn                    = 'trainoss';     % One-step secant backpropagation
+    net.divideFcn                   = 'dividerand';   % data division random
+    net.divideParam .trainRatio     = 0.85;              % all data used for training
+    net.divideParam.valRatio        = 0.15;
+    net.divideParam.testRatio       = 0;
+    net.trainParam.epochs           = 100;           % maximum number of epochs to train (for early stopping)
+    net.trainParam.showCommandLine  = false;          % do not generate command-line output
+    net.trainParam.showWindow       = true;           % show training GUI
+    net.trainParam.max_fail         = 10;             % maximum validation failures (for early stopping)
+    net.trainParam.show             = 1;              % Epochs between displays
+    % train net
+    [net,record] = train(net,X,Y);
+    % save network and performance
+    save(strcat('net_feedfor_Rho',num2str(h),'.mat'),'net')
+    save(strcat('perf_feedfor_Rho',num2str(h),'.mat'),'record')
+end
--- 2_code_training/train_NARNET_Boundary.m
+++ 2_code_training/train_NARNET_Boundary.m
@@ -16,40 +16,72 @@ data = cellfun(@(x) (reshape(x, [], 1)),BoundaryDescriptors,'UniformOutput',fals
 
 seqLen = size(data,2);
 samples = size(data,1);
+maxSam = floor(samples*0.85);
 
+% define recurrent neural net of type NARNET
 trainFcn = 'trainoss';
 feedbackDelays = 1:seqLen-1;
-hiddenLayerSize = 200;
+hiddenLayerSize = 100;
+net = narnet(feedbackDelays,hiddenLayerSize,'open',trainFcn);
+net = removedelay(net);
+net.trainParam.epochs   = 2;
+net.divideFcn           = 'dividerand'; 
+net.divideMode          = 'time';
+net.performFcn          = 'mse';  
+net.input.processFcns   = {'removeconstantrows','mapminmax'};
+net.divideParam.trainRatio  = 1;
+net.trainParam.showWindow   = false;           
+net.trainParam.min_grad = 0;
 
+% set apart a random set of samples for validation
+valRatio = .15;                        
+vIdx = randi([1 samples],1,round(samples*valRatio)); 
+trainSet = data;    
+trainSet(vIdx,:) = []; 
+trainSet = trainSet(randperm(size(trainSet,1)),:);
+valSet = data(vIdx,:);
+target = valSet(:,end);
 
-epochs = [10 20 50 100 200 400 linspace(600,floor(samples*0.85),10)];
-netPerfor  = zeros(numel(epochs),1);
-basePerfor = zeros(numel(epochs),1);
+% train different model with a dataset of increasing size
+epochs = [20 50 100 200 400 round(linspace(600,maxSam,11))];
+netPerf  = zeros(numel(epochs),1);
+
+start = 1;
 for m = 1:numel(epochs)
-    valRatio = .15;                        
-    vIdx = randi([1 samples],1,round(samples*valRatio)); 
-    trainSet = data;    
-    trainSet(vIdx,:) = []; 
-    trainSet = trainSet(randperm(size(trainSet,1)),:);
-    valSet   = data(vIdx,:);
+    T = trainSet(start:epochs(m),:);
+    for i = 1:size(T,1)
+        [x,xi,ai,t] = preparets(net,{},{},T(i,:));
+        net = train(net,x,t,xi,ai);
+    end
+    start = epochs(m)+1;
     
-    net = narnet(feedbackDelays,hiddenLayerSize,'open',trainFcn);
-    net = removedelay(net);
-    net.trainParam.epochs   = 2;
-    net.divideFcn           = 'dividerand'; 
-    net.divideMode          = 'time';
-    net.performFcn          = 'mse';  
-    net.input.processFcns   = {'removeconstantrows','mapminmax'};
-    net.divideParam.trainRatio  = 1;
-    net.trainParam.showWindow   = false;           
-    net.trainParam.min_grad = 0;
-           
-    for i = 1:epochs(m)
+    % precit and performance
+    output = [];
+    for j = 1:size(valSet,1)
+        query = valSet(j,1:end-1);
+        [xq,xiq,aiq,tq] = preparets(net,{},{},query);
+        output = [output ; net(xq,xiq,aiq)];
+    end
+    netPerf(m) = sqrt(mse(gsubtract(target,output)));
+end
+
+preFrame = valSet(:,end-1);
+basePerf = sqrt(mse(gsubtract(target,preFrame)));
+
+boundary_performance = mat2str([netPerf ones(size(netPerf))*basePerf]);
+save('perf_Boundary.mat','boundary_performance')
+
+% Increasing number of epochs
+
+epochs = 50;
+netPerf  = zeros(epochs,1);
+for m = 1:50
+    for i = 1:size(trainSet,1)
         T = trainSet(i,:);
         [x,xi,ai,t] = preparets(net,{},{},T);
         net = train(net,x,t,xi,ai);
     end
-    
+
     % precit and performance
     output = [];
     for j = 1:size(valSet,1)
@@ -57,60 +89,115 @@ for m = 1:numel(epochs)
         [xq,xiq,aiq,tq] = preparets(net,{},{},query);
         output = [output ; net(xq,xiq,aiq)];
     end
-    target = valSet(:,end);
-    preFrame = valSet(:,end-1);
-    netPerfor(m) = sqrt(mse(gsubtract(target,output)));
-    basePerfor(m) = sqrt(mse(gsubtract(target,preFrame)));
+    netPerf(m) = sqrt(mse(gsubtract(target,output)));
 end
 
-boundary_performance = mat2str([netPerfor basePerfor]);
-save('perf_Boundary.mat','boundary_performance')
+rho_performance = mat2str([netPerf ones(size(netPerf))*basePerf]);
+save('perf_Boundary_1to50Epochs.mat','boundary_performance')
+save('net_Boundary_narnet_50epochs.mat','net')
 
-%% Increasing number of Epochs
-
-epochs = linspace(1,10,10);
-netPerfor  = zeros(numel(epochs),1);
-basePerfor = zeros(numel(epochs),1);
-for m = 1:numel(epochs)
-    valRatio = .15;                        
-    vIdx = randi([1 samples],1,round(samples*valRatio)); 
-    trainSet = data;    
-    trainSet(vIdx,:) = []; 
-    trainSet = trainSet(randperm(size(trainSet,1)),:);
-    valSet   = data(vIdx,:);
-    
-    net = narnet(feedbackDelays,hiddenLayerSize,'open',trainFcn);
-    net = removedelay(net);
-    net.trainParam.epochs   = 2;
-    net.divideFcn           = 'dividerand'; 
-    net.divideMode          = 'time';
-    net.performFcn          = 'mse';  
-    net.input.processFcns   = {'removeconstantrows','mapminmax'};
-    net.divideParam.trainRatio  = 1;
-    net.trainParam.showWindow   = false;           
-    net.trainParam.min_grad = 0;
-    % number of epochs to train with full training set, from 1 to 10.
-    for k = 1:epochs(m)       
-        for i = 1:size(trainSet,1)
-            T = trainSet(i,:);
-            [x,xi,ai,t] = preparets(net,{},{},T);
-            net = train(net,x,t,xi,ai);
-        end
-    end
-    
-    % precit and performance
-    output = [];
-    for j = 1:size(valSet,1)
-        query = valSet(j,1:end-1);
-        [xq,xiq,aiq,tq] = preparets(net,{},{},query);
-        output = [output ; net(xq,xiq,aiq)];
-    end
-    target = valSet(:,end);
-    preFrame = valSet(:,end-1);
-    netPerfor(m) = sqrt(mse(gsubtract(target,output)));
-    basePerfor(m) = sqrt(mse(gsubtract(target,preFrame)));
-end
-
-boundary_performance = mat2str([netPerfor basePerfor]);
-save('net_Boundary_narnet.mat','net')
-save('perf_Boundary_epochs.mat','boundary_performance')
+% 
+% 
+% 
+% 
+% %%
+% seqLen = size(data,2);
+% samples = size(data,1);
+% 
+% trainFcn = 'trainoss';
+% feedbackDelays = 1:seqLen-1;
+% hiddenLayerSize = 200;
+% 
+% 
+% epochs = [10 20 50 100 200 400 linspace(600,floor(samples*0.85),10)];
+% netPerfor  = zeros(numel(epochs),1);
+% basePerfor = zeros(numel(epochs),1);
+% for m = 1:numel(epochs)
+%     valRatio = .15;                        
+%     vIdx = randi([1 samples],1,round(samples*valRatio)); 
+%     trainSet = data;    
+%     trainSet(vIdx,:) = []; 
+%     trainSet = trainSet(randperm(size(trainSet,1)),:);
+%     valSet   = data(vIdx,:);
+%     
+%     net = narnet(feedbackDelays,hiddenLayerSize,'open',trainFcn);
+%     net = removedelay(net);
+%     net.trainParam.epochs   = 2;
+%     net.divideFcn           = 'dividerand'; 
+%     net.divideMode          = 'time';
+%     net.performFcn          = 'mse';  
+%     net.input.processFcns   = {'removeconstantrows','mapminmax'};
+%     net.divideParam.trainRatio  = 1;
+%     net.trainParam.showWindow   = false;           
+%     net.trainParam.min_grad = 0;
+%            
+%     for i = 1:epochs(m)
+%         T = trainSet(i,:);
+%         [x,xi,ai,t] = preparets(net,{},{},T);
+%         net = train(net,x,t,xi,ai);
+%     end
+%     
+%     % precit and performance
+%     output = [];
+%     for j = 1:size(valSet,1)
+%         query = valSet(j,1:end-1);
+%         [xq,xiq,aiq,tq] = preparets(net,{},{},query);
+%         output = [output ; net(xq,xiq,aiq)];
+%     end
+%     target = valSet(:,end);
+%     preFrame = valSet(:,end-1);
+%     netPerfor(m) = sqrt(mse(gsubtract(target,output)));
+%     basePerfor(m) = sqrt(mse(gsubtract(target,preFrame)));
+% end
+% 
+% boundary_performance = mat2str([netPerfor basePerfor]);
+% save('perf_Boundary.mat','boundary_performance')
+% 
+% %% Increasing number of Epochs
+% 
+% epochs = linspace(1,10,10);
+% netPerfor  = zeros(numel(epochs),1);
+% basePerfor = zeros(numel(epochs),1);
+% for m = 1:numel(epochs)
+%     valRatio = .15;                        
+%     vIdx = randi([1 samples],1,round(samples*valRatio)); 
+%     trainSet = data;    
+%     trainSet(vIdx,:) = []; 
+%     trainSet = trainSet(randperm(size(trainSet,1)),:);
+%     valSet   = data(vIdx,:);
+%     
+%     net = narnet(feedbackDelays,hiddenLayerSize,'open',trainFcn);
+%     net = removedelay(net);
+%     net.trainParam.epochs   = 2;
+%     net.divideFcn           = 'dividerand'; 
+%     net.divideMode          = 'time';
+%     net.performFcn          = 'mse';  
+%     net.input.processFcns   = {'removeconstantrows','mapminmax'};
+%     net.divideParam.trainRatio  = 1;
+%     net.trainParam.showWindow   = false;           
+%     net.trainParam.min_grad = 0;
+%     % number of epochs to train with full training set, from 1 to 10.
+%     for k = 1:epochs(m)       
+%         for i = 1:size(trainSet,1)
+%             T = trainSet(i,:);
+%             [x,xi,ai,t] = preparets(net,{},{},T);
+%             net = train(net,x,t,xi,ai);
+%         end
+%     end
+%     
+%     % precit and performance
+%     output = [];
+%     for j = 1:size(valSet,1)
+%         query = valSet(j,1:end-1);
+%         [xq,xiq,aiq,tq] = preparets(net,{},{},query);
+%         output = [output ; net(xq,xiq,aiq)];
+%     end
+%     target = valSet(:,end);
+%     preFrame = valSet(:,end-1);
+%     netPerfor(m) = sqrt(mse(gsubtract(target,output)));
+%     basePerfor(m) = sqrt(mse(gsubtract(target,preFrame)));
+% end
+% 
+% boundary_performance = mat2str([netPerfor basePerfor]);
+% save('net_Boundary_narnet.mat','net')
+% save('perf_Boundary_epochs.mat','boundary_performance')
